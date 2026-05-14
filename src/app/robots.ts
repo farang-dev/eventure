bade1d71c86a78a1c22788f2e9a981ae9cbc9b34
Masafumi Nozawa <masafumi.nozawa.q@gmail.com>
2026-05-14 15:04:52 +0300

    added : gtag

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://eventure-cyan.vercel.app/sitemap.xml',
+    sitemap: 'https://www.eventurer.online/sitemap.xml',
   }
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
  
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://eventure-cyan.vercel.app'
+  const baseUrl = 'https://www.eventurer.online'
   const cities = ['tokyo', 'osaka', 'london', 'vilnius', 'belgrade', 'tbilisi']
   
   const cityUrls = cities.map((city) => ({
